d039313bbe6b46433ff1ef7b98e82f5ee31150f0
Gia Phan <guphan@damudo.at>
2023-10-04 09:39:28 +0200

    test

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -3,6 +3,7 @@ export default function Orders() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           <h1>Orders</h1>
+          <h1>Test</h1>
         </div>
       </main>
     )
