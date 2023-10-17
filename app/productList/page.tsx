2c00db8cc3fabe4f541d832b160d2493fe22e8e6
Gia-Uy Phan <ITN263376@qualifizierung.at>
2023-10-17 10:51:26 +0200

    DB no-cache prop added

--- app/productList/page.tsx
+++ app/productList/page.tsx
@@ -7,6 +7,7 @@ import ProductListSearchTable from "@/components/productList/productListTable/pr
 
     const records = await pb.collection('Products').getFullList({
       sort: '-created',
+      cache: 'no-store' 
   });
     return records;
 
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,27 +1,28 @@
 import { POCKET_BASE_URL } from "@/db";
-import PocketBase from 'pocketbase';
+import PocketBase from "pocketbase";
 import ProductGrid from "@/components/product/productGrid";
 interface Props {
-    params: {
-      id: string;
-    };
-  }
+  params: {
+    id: string;
+  };
+}
 
-  async function getProducts({ params }: Props){
-    const pb = new PocketBase(POCKET_BASE_URL);
+async function getProducts({ params }: Props) {
+  const pb = new PocketBase(POCKET_BASE_URL);
 
-    const record = await pb.collection('Products').getFullList({
-      sort: '-created',
-      filter: `category = "${params.id}" `
+  const record = await pb.collection("Products").getFullList({
+    sort: "-created",
+    filter: `category = "${params.id}" `,
+    cache: 'no-store' 
   });
-    console.log(record)
-    return record;
 
-  }
 
-  export default async function Products({ params }: Props) {
-    const data = await getProducts({params});
-    
-    return <ProductGrid product={data}/>
-    
-  }
+  console.log(record);
+  return record;
+}
+
+export default async function Products({ params }: Props) {
+  const data = await getProducts({ params });
+
+  return <ProductGrid product={data} />;
+}
